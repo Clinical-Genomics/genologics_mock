42c906cb65dd576b0c5f33a4c81e516341a5d44a
mayabrandi <maya.brandi@scilifelab.se>
2020-11-04 08:10:17 +0100

    adding arguments and restructure

--- genologics_mock/entities.py
+++ genologics_mock/entities.py
@@ -1,36 +1,8 @@
-
-
-
-class MockProcess:
-    def __init__(self, date_run='2018-01-01', process_type=None, pid=None, modified=None, input_output_maps=[]):
-        self.date_run = date_run
-        self.type = process_type
-        self.udf = {}
-        self.input_artifact_list = []
-        self.id = pid
-        self.outputs = []
-        self.inputs = []
-        self.input_output_maps = input_output_maps
-        self.modified = modified
-
-    def all_outputs(self, unique=False):
-        
-        if unique:
-            return list(set(self.outputs))
-        return self.outputs
-
-    def all_inputs(self, unique=False):
-        
-        if unique:
-            return list(set(self.inputs))  
-        return self.inputs
-
-    def __repr__(self):
-        return f"Process:date_run={self.date_run},type={self.type}"
+from typing import List
 
 
 class MockProcessType:
-    def __init__(self, name=''):
+    def __init__(self, name=""):
         self.name = name
 
     def __repr__(self):
@@ -38,7 +10,7 @@ class MockProcessType:
 
 
 class MockContainerType:
-    def __init__(self, name=''):
+    def __init__(self, name=""):
         self.name = name
 
     def __repr__(self):
@@ -46,7 +18,7 @@ class MockContainerType:
 
 
 class MockContainer:
-    def __init__(self, name='', type=MockContainerType()):
+    def __init__(self, name="", type=MockContainerType()):
         self.name = name
         self.type = type
 
@@ -55,8 +27,17 @@ class MockContainer:
 
 
 class MockArtifact:
-    def __init__(self, parent_process=None, samples=None, id=None, location=(), udf={},
-                 qc_flag='UNKNOWN', reagent_labels=[], type=None):
+    def __init__(
+        self,
+        parent_process=None,
+        samples=None,
+        id=None,
+        location=(),
+        udf={},
+        qc_flag="UNKNOWN",
+        reagent_labels=[],
+        type=None,
+    ):
         self.id = id
         self.location = location
         self.parent_process = parent_process
@@ -78,7 +59,7 @@ class MockArtifact:
 
 
 class MockSample:
-    def __init__(self, sample_id='sample', udfs={}, artifact=MockArtifact()):
+    def __init__(self, sample_id="sample", udfs={}, artifact=MockArtifact()):
         self.id = sample_id
         self.udf = udfs
         self.artifact = artifact
@@ -87,12 +68,51 @@ class MockSample:
         return f"Sample:id={self.id},udf={self.udf}"
 
 
-class MockReagentLabel():
-    def __init__(self, name='IDT_10nt_NXT_109', sequence='TAGGAAGCGG-CCTGGATTGG',
-                 category='Illumina IDT'):
+class MockReagentLabel:
+    def __init__(
+        self,
+        name="IDT_10nt_NXT_109",
+        sequence="TAGGAAGCGG-CCTGGATTGG",
+        category="Illumina IDT",
+    ):
         self.name = name
         self.sequence = sequence
         self.category = category
 
     def __repr__(self):
         return f"ReagentLabel:name={self.name},category={self.category}"
+
+
+class MockProcess:
+    def __init__(
+        self,
+        date_run: str = "2018-01-01",
+        process_type: MockProcessType = None,
+        udf: dict = {},
+        input_artifact_list: List[MockArtifact] = [],
+        pid: str = None,
+        outputs: List[MockArtifact]=[],
+        input_output_maps: list = [],
+    ):
+        self.date_run = date_run
+        self.type = process_type
+        self.udf = udf
+        self.input_artifact_list = input_artifact_list
+        self.id = pid
+        self.outputs = outputs
+        self.input_output_maps = input_output_maps
+
+    def all_outputs(self, unique=False):
+
+        if unique:
+            return list(set(self.outputs))
+        return self.outputs
+
+    def all_inputs(self, unique=False):
+
+        if unique:
+            return list(set(self.input_artifact_list))
+        return self.input_artifact_list
+
+    def __repr__(self):
+        return f"Process:date_run={self.date_run},type={self.type}"
